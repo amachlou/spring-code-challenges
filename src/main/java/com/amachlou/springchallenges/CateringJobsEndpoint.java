031d5aae73c0ac1ecc850082e83f7e4d6982f42b
amachlou <abderrahim.machlou@dxc.com>
2023-12-20 17:01:24 +0100

    Exposing Custom Actuator Endpoint

--- src/main/java/com/amachlou/springchallenges/CateringJobsEndpoint.java
+++ src/main/java/com/amachlou/springchallenges/CateringJobsEndpoint.java
@@ -1,12 +1,23 @@
 package com.amachlou.springchallenges;
 
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Component
+@Endpoint(id = "catering-jobs")
 public class CateringJobsEndpoint {
     private final CateringJobRepository cateringJobRepository;
 
     public CateringJobsEndpoint(CateringJobRepository cateringJobRepository) {
         this.cateringJobRepository = cateringJobRepository;
+    }
+
+    @ReadOperation
+    Map<Status, Long> cateringJobs(){
+        return cateringJobRepository.findAll().stream().collect(Collectors.groupingBy(CateringJob::getStatus, Collectors.counting()));
     }
 }
