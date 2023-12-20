ba069f67aa556358aed4703cfd3f00fa96d76895
amachlou <abderrahim.machlou@dxc.com>
2023-12-20 16:11:14 +0100

    Schaduling Tast

--- src/main/java/com/amachlou/springchallenges/ScheduledTasks.java
+++ src/main/java/com/amachlou/springchallenges/ScheduledTasks.java
@@ -2,13 +2,23 @@ package com.amachlou.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
+
+    @Autowired
+    CateringJobRepository cateringJobRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
+    @Scheduled(cron = "*/10 * * * * *")
     public void reportOrderStats() {
-        
+        List<CateringJob> jobs = cateringJobRepository.findAll();
+        logger.info("We have {} Jobs", jobs.size());
     }
 }
