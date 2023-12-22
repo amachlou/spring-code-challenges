a32c20ff1b33eb5143977f239ce39d232942b0c8
amachlou <abderrahim.machlou@dxc.com>
2023-12-22 13:07:13 +0100

    AOP, Intercepting requests

--- src/main/java/com/amachlou/springchallenges/LoggingAspect.java
+++ src/main/java/com/amachlou/springchallenges/LoggingAspect.java
@@ -1,10 +1,28 @@
 package com.amachlou.springchallenges;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.configuration.xml.JobParserJobFactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class LoggingAspect {
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.amachlou.springchallenges.CateringJobController.*ById(Long)) && args(id)")
+    public Object befoireReq(ProceedingJoinPoint proceedingJoinPoint, Long id) throws Throwable {
+        logger.info("Invoked Method: {}", proceedingJoinPoint.getSignature().getName());
+        logger.info("CateringJob id: {}", id);
+        Object result = proceedingJoinPoint.proceed();
+        logger.info("Response id: {}", result);
+        return result;
+    }
+
 }
